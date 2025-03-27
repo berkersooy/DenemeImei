39156285dbb83c4b1cce18536f0f804f715ac7bc
Berk Ersoy <berkersooy@gmail.com>
2025-03-27 14:43:10 +0300

    deneme3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,11 @@ version = "1.0.0"
 
 android {
     namespace = "com.example.deviceutils"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         minSdk = 26
-        targetSdk = 34
-
+        targetSdk = 33
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -32,10 +31,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.5.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
